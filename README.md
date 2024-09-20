d648ba693dc64b8e521f8a42c1c55afa6c9cb6f2
alishba-khan2005 <alishbaferozkhan001@gmail.com>
2024-09-20 01:43:13 -0700

    loop

---
+++ README.md
@@ -0,0 +1,1 @@
+"# if-nested-loop1" 
---
+++ nested loop 1.py
@@ -0,0 +1,14 @@
+
+print("---------scholarship program----------")
+gpa=float(input("enter your GPA:  "))
+if gpa>=3.3:
+        print("scholarship approved")
+        if gpa>=3.3:
+            print("you have got scholarship in 1st semester")
+            if gpa>=3.6:
+                  print("you have got scholarship in 2nd semester")
+                  if gpa>=4.00:
+                        print("you have got scholarship 3rd semester")
+else:
+          print("you did'nt got the scholarship")
+          
